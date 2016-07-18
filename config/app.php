cc7a821892b3dfb701795d2ac41071fd94847680
Dejan Ristic <dejanr77@yahoo.com>
2016-07-18 19:41:51 +0200

    added package ('Intervention/image')

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Cviebrock\EloquentSluggable\ServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
+        Intervention\Image\ImageServiceProvider::class,
     ],
 
     /*
@@ -209,6 +211,8 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
