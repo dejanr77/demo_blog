4d215a8995b073241fa8e3b4d396c3603ed20546
Dejan Ristic <dejanr77@yahoo.com>
2016-07-26 09:40:01 +0200

    added a helper function named 'set_action'.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-4 col-md-offset-4">
                 <h1 class="page-header">
                     Register
-                    <a class="btn btn-default btn-sm " href="{{ url()->previous() }}">back</a>
+                    <a class="btn btn-default btn-sm " href="{{ url('/') }}">home</a>
                 </h1>
                 <form role="form" method="POST" action="{{ url('/register') }}">
                     {{ csrf_field() }}
