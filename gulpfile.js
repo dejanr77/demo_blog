3b8d8196fed48224bd00345c2f6bb1b568ce6dee
Dejan Ristic <dejanr77@yahoo.com>
2016-08-29 01:08:10 +0200

    assets management ( public layout ).

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
-var elixir = require('laravel-elixir');
-
+var gulp = require('gulp'),
+    elixir = require('laravel-elixir'),
+    paths = {
+        'jquery': 'node_modules/jquery/dist/',
+        'bootstrap': 'node_modules/bootstrap-sass/assets/',
+        'font_awesome': 'node_modules/font-awesome/',
+        'bootststrap-sass': 'node_modules/assets/'
+    };
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -11,6 +17,41 @@ var elixir = require('laravel-elixir');
  |
  */
 
+elixir.config.sourcemaps = false;
+
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+    mix.copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/build/fonts/bootstrap');
+    mix.copy(paths.font_awesome + 'fonts/**', 'public/build/fonts');
+
+    mix.copy(paths.font_awesome + 'css/font-awesome.css', 'resources/assets/css/font-awesome.css');
+
+    mix.copy('assets/js/article.js','public/js/article.js');
+
+    mix.copy(paths.jquery + 'jquery.js', 'resources/assets/js/jquery.js');
+    mix.copy(paths.bootstrap + 'javascripts/bootstrap.js', 'resources/assets/js/bootstrap.js');
+
+
+    mix.sass('app.scss','resources/assets/css');
+
+
+    mix.styles([
+        '../../' + 'assets/css/app.css',
+        '../../' + 'assets/css/font-awesome.css',
+        '../../' + 'assets/css/clean-blog.css',
+        '../../' + 'assets/css/style.css'
+    ], 'public/css/demo_blog.css');
+
+
+    mix.scripts([
+        '../../' + 'assets/js/jquery.js',
+        '../../' + 'assets/js/bootstrap.js',
+        '../../' + 'assets/js/clean-blog.js'
+    ], 'public/js/demo_blog.js');
+
+
+    mix.version([
+        'public/css/demo_blog.css',
+        'public/js/demo_blog.js'
+    ]);
 });
--- resources/views/public/partials/head.blade.php
+++ resources/views/public/partials/head.blade.php
@@ -6,14 +6,10 @@
 
 <title>@yield('title')</title>
 @section('style')
-<!-- Bootstrap Core CSS -->
-<link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet">
-<!-- Custom CSS -->
-<link href="{{ url('css/clean-blog.css') }}" rel="stylesheet">
-<link href="{{ url('css/style.css') }}" rel="stylesheet">
+
+<link href="{{ url(elixir('css/demo_blog.css')) }}" rel="stylesheet">
 @show
 <!-- Custom Fonts -->
-<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
